162823f809cf06a5838c0c86e19cda1c5a0ee203
franciszeklasinski <122890262+franciszeklasinski@users.noreply.github.com>
2023-01-30 18:20:41 +0100

    Add files via upload

---
+++ e544bf1ec2902f20473a7011f10bc405.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int bezwzgledna(int n)
+{
+	if(n<0)
+	{
+		n=n*(-1);
+	}
+return n;
+}
+
+int main()
+{
+	
+	int n;
+	cin>>n;
+	cout<<"Wartosc bezwzgledna liczby: "<<n<<" wynosi : "<<bezwzgledna(n);
+	
+	
+	
+	
+	return 0;
+}
